a047b65fe465a2e21bfea369e4dbf0e1d0904658
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-01-06 21:31:16 +0900

    add: ListAddコンポーネント

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,48 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-export default new Vuex.Store({
+const savedLists = localStorage.getItem("trello-lists"); // ★①追加
+const store = new Vuex.Store({
+  // ★②編集
   state: {
+    // ★ここから③編集
+    lists: savedLists
+      ? JSON.parse(savedLists)
+      : [
+          {
+            title: "Backlog",
+            cards: [{ body: "English" }, { body: "Mathematics" }],
+          },
+          {
+            title: "Todo",
+            cards: [{ body: "Science" }],
+          },
+          {
+            title: "Doing",
+            cards: [],
+          },
+        ],
+    // ★ここまで③編集
   },
   mutations: {
+    addlist(state, payload) {
+      state.lists.push({ title: payload.title, cards: [] });
+    },
   },
   actions: {
+    addlist(context, payload) {
+      context.commit("addlist", payload);
+    },
   },
-  modules: {
-  }
-})
+  getters: {},
+});
+
+// ★ここから追記
+store.subscribe((mutation, state) => {
+  localStorage.setItem("trello-lists", JSON.stringify(state.lists));
+});
+
+export default store;
+// ★ここまで追記
