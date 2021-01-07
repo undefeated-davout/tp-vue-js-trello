ac3d5829f78369bf80d03424431261f3d5205ebb
undefeated-davout <34504518+undefeated-davout@users.noreply.github.com>
2021-01-07 16:55:41 +0900

    add: Listドラッグ&ドロップ

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -5,7 +5,7 @@
     </header>
     <main>
       <p class="info-line">All: {{ totalCardCount }} tasks</p>
-      <div class="list-index">
+      <draggable :list="lists" @end="movingList" class="list-index">
         <list
           v-for="(item, index) in lists"
           :key="item.id"
@@ -15,12 +15,13 @@
           @change="movingCard"
         />
         <list-add />
-      </div>
+      </draggable>
     </main>
   </div>
 </template>
 
 <script>
+import draggable from "vuedraggable";
 import List from "./List";
 import ListAdd from "./ListAdd";
 import { mapState } from "vuex";
@@ -29,6 +30,7 @@ export default {
   components: {
     ListAdd,
     List,
+    draggable,
   },
   computed: {
     ...mapState(["lists"]),
@@ -38,6 +40,9 @@ export default {
   },
   methods: {
     movingCard: function() {
+      this.$store.dispatch("updateList", { lists: this.lists });
+    },
+    movingList: function() {
       this.$store.dispatch("updateList", { lists: this.lists });
     },
   },
